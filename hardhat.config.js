1faf9926179a023294fd165a86266743d221d4e7
Nick Barry <itsnickbarry@protonmail.ch>
2021-09-02 01:37:42 -0500

    use hardhat-etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,6 +34,10 @@ module.exports = {
     runOnCompile: true,
   },
 
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
+
   gasReporter: {
     enabled: process.env.REPORT_GAS === 'true',
   },
