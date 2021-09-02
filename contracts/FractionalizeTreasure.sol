4e50aae353e5f7d5a67701400390a2bdd0454b79
ymoxley <ymoxley@gmail.com>
2021-09-02 16:42:34 -0500

    mintBatch

--- contracts/FractionalizeTreasure.sol
+++ contracts/FractionalizeTreasure.sol
@@ -2,6 +2,24 @@ pragma solidity ^0.8.0
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
+interface IERC721Receiver {
+    /**
+     * @dev Whenever an {IERC721} `tokenId` token is transferred to this contract via {IERC721-safeTransferFrom}
+     * by `operator` from `from`, this function is called.
+     *
+     * It must return its Solidity selector to confirm the token transfer.
+     * If any other value is returned or the interface is not implemented by the recipient, the transfer will be reverted.
+     *
+     * The selector can be obtained in Solidity with `IERC721.onERC721Received.selector`.
+     */
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
+
 interface TreasureInterface {
     function ownerOf(uint256 tokenId) external view returns (address owner);
     
@@ -22,12 +40,12 @@ interface TreasureInterface {
     function getAsset8(uint256 tokenId) public view returns (string memory);
 }
 
+
+
 contract FractionalizeTreasure is ERC1155, ReentrancyGuard, Ownable {
     
     address public treasureAddress = 0xf3DFbE887D81C442557f7a59e3a0aEcf5e39F6aa;
     TreasureInterface public treasureContract = TreasureInterface(treasureAddress);
-    
-    mapping assetToTokenId (string=>uint256);
     
     struct Item {
     string name;
@@ -50,8 +68,13 @@ contract FractionalizeTreasure is ERC1155, ReentrancyGuard, Ownable {
 
     
     function decompose(uint256 tokenId) public returns (string[] memory) {
-        _mint(msg.sender,uint256(keccak256(getAsset1(itemValues[tokenId]))),1,itemNames[tokenId])
-        
-        _mint(msg.sender,uint256(keccak256(getAsset2(itemValues[tokenId]))))
+        _mintBatch(msg.sender,uint256[keccak256(itemNames[getAsset1(tokenId)],keccak256(itemNames[getAsset2(tokenId)]))])
+    }
+    function fractionalize (uint tokenId) external {
+    // TODO: ERC721 transfer of tokenId to address(this)
+    safeTransferFrom(msg.sender, this, tokenId)
+
+    _mintBatch(msg.sender, uint256[uint256(keccak256(getAsset1(tokenId))),uint256(keccak256(getAsset2(tokenId))),uint256(keccak256(getAsset3(tokenId))),uint256(keccak256(getAsset4(tokenId))),uint256(keccak256(getAsset5(tokenId))),uint256(keccak256(getAsset6(tokenId))),uint256(keccak256(getAsset7(tokenId))),uint256(keccak256(getAsset8(tokenId))), uint256[1,1,1,1,1,1,1,1]);
+    
     }
 }
