5c1fa12787776a96e4bd32013af40479c3b132e7
ymoxley <ymoxley@gmail.com>
2021-09-13 21:42:09 -0500

    decimal fix for setRewardParams

--- tasks/set_reward_params.js
+++ tasks/set_reward_params.js
@@ -1,7 +1,7 @@
 const deployments = require('../data/deployments');
 
 task('reward-params')
-  .addParam('amount')
+  .addParam('amount', '18-decimal formatted')
   .addParam('duration')
   .setAction(async function ({ amount, duration }) {
     const [sender] = await ethers.getSigners();
@@ -14,6 +14,8 @@ task('reward-params')
     // duration should be: 118125 blocks
     // COMMENT: @NickBarry do I need to do anything special to this amount? I would like to make sure we are emitting 166k tokens in the first week.
     // Does it need to be passed as a big number?
-    const tx = await instance.connect(sender).setRewardParams(amount, duration);
+    const tx = await instance
+      .connect(sender)
+      .setRewardParams(ethers.utils.parseUnits(amount, 18), duration);
     await tx.wait();
   });
