7b1c1bbdb4908bc1a0738a5092e2bb1e7e942b63
Nick Barry <itsnickbarry@protonmail.ch>
2021-09-13 21:18:42 -0500

    whitelist farms and multisig after whitelist fix deployment

--- tasks/magic_fix_whitelist.js
+++ tasks/magic_fix_whitelist.js
@@ -39,4 +39,21 @@ task('magic-fix-whitelist').setAction(async function () {
     .connect(deployer)
     .diamondCut(facetCuts, ethers.constants.AddressZero, '0x');
   await cutTx.wait();
+
+  const magicWhitelist = await ethers.getContractAt(
+    'MagicWhitelist',
+    deployments.magic,
+  );
+
+  await Promise.all([
+    await magicWhitelist.connect(deployer).addToWhitelist(deployments.agldFarm),
+    await magicWhitelist.connect(deployer).addToWhitelist(deployments.lootFarm),
+    await magicWhitelist
+      .connect(deployer)
+      .addToWhitelist(deployments.treasureFarm),
+    await magicWhitelist.connect(deployer).addToWhitelist(deployments.nFarm),
+    await magicWhitelist
+      .connect(deployer)
+      .addToWhitelist('0xEc834bD1F492a8Bd5aa71023550C44D4fB14632A'),
+  ]);
 });
