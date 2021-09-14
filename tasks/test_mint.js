82e37872ae3aeec0b57663b93511c65338788dc5
ymoxley <ymoxley@gmail.com>
2021-09-14 14:05:38 -0500

    tested whitelist

--- tasks/test_mint.js
+++ tasks/test_mint.js
@@ -2,23 +2,13 @@ const fs = require('fs');
 const deployments = require('../data/deployments');
 
 task('magic-mint').setAction(async function () {
-  const impersonate = await hre.network.provider.request({
-    method: 'hardhat_impersonateAccount',
-    params: ['0xdCf809BBa1d9ee75c6991f1E2974163cF2555418'],
-  });
-
-  const deployer = await hre.ethers.getSigner(
-    '0xdCf809BBa1d9ee75c6991f1E2974163cF2555418',
-  );
+  const [deployer] = await ethers.getSigners();
 
   console.log(deployer.address);
-  const oldMagicImplementation = await ethers.getContractAt(
-    'Magic',
-    deployments.magicImplementation,
-  );
 
-  const mint = await oldMagicImplementation
-    .connect(deployer)
-    .mint(deployer.address, 10000);
+  // MagicProxy address + Magic ABI
+  const magic = await ethers.getContractAt('Magic', deployments.magic);
+
+  const mint = await magic.connect(deployer).mint(deployer.address, 10000);
   await mint.wait();
 });
