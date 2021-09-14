8e0ea75a0e038bd1ed269757cfa1a566ed91fea9
ymoxley <ymoxley@gmail.com>
2021-09-14 15:14:16 -0500

    removed impersonate

--- tasks/add_whitelist.js
+++ tasks/add_whitelist.js
@@ -2,13 +2,7 @@ const fs = require('fs');
 const deployments = require('../data/deployments');
 
 task('add-whitelist').setAction(async function () {
-  const impersonate = await hre.network.provider.request({
-    method: 'hardhat_impersonateAccount',
-    params: ['0x2940108780B870Ae8f1509Be95Fdf2b6fb066E41'],
-  });
-  const deployer = await hre.ethers.getSigner(
-    '0x2940108780B870Ae8f1509Be95Fdf2b6fb066E41',
-  );
+  const [deployer] = await ethers.getSigners();
 
   const magicWhitelist = await ethers.getContractAt(
     'MagicWhitelist',
