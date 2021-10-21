9f17411d9535618ece2656496c2136dd4031b78e
Nick Barry <itsnickbarry@protonmail.ch>
2021-10-21 00:26:34 -0500

    add minter add and remove functions to CardsMint

--- contracts/arbitrum/cards/CardsMint.sol
+++ contracts/arbitrum/cards/CardsMint.sol
@@ -2,11 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+import '@solidstate/contracts/access/OwnableInternal.sol';
 import '@solidstate/contracts/token/ERC1155/enumerable/ERC1155EnumerableInternal.sol';
 
 import './CardsMintStorage.sol';
 
-contract CardsMint is ERC1155EnumerableInternal {
+contract CardsMint is ERC1155EnumerableInternal, OwnableInternal {
     function mint(
         address account,
         uint256 tokenId,
@@ -17,5 +18,13 @@ contract CardsMint is ERC1155EnumerableInternal {
             'CardsMint: unauthorized minter'
         );
         _mint(account, tokenId, amount, '');
+    }
+
+    function addMinter(address account) external onlyOwner {
+        CardsMintStorage.layout().minters[account] = true;
+    }
+
+    function removeMinter(address account) external onlyOwner {
+        CardsMintStorage.layout().minters[account] = false;
     }
 }
