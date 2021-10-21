d8e2f5c51a699d74f1e0e338c1ccdddbd2eb5f7d
Nick Barry <itsnickbarry@protonmail.ch>
2021-10-20 19:20:23 -0500

    upgrade dependencies, override uri function

--- contracts/arbitrum/cards/CardsBase.sol
+++ contracts/arbitrum/cards/CardsBase.sol
@@ -2,7 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import '@solidstate/contracts/introspection/ERC165.sol';
-import '@solidstate/contracts/token/ERC1155/enumerable/ERC1155Enumerable.sol';
+import '@solidstate/contracts/token/ERC1155/ERC1155.sol';
 
-contract CardsBase is ERC1155Enumerable, ERC165 {}
+contract CardsBase is ERC1155 {
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(super.uri(tokenId), '.json'));
+    }
+}
